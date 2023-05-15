a67e83421a5a66c97e36dfcf1330e402d0cd8b87
sanikani <sanikani@naver.com>
2023-05-15 19:51:06 +0900

    init

---
+++ File upload/flask_upload.py
@@ -0,0 +1,43 @@
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from flask import Flask, render_template, request, jsonify
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+model = tf.keras.models.load_model('keras_model.h5')
+
+
+class_names = ['conjunctivitis', 'Normal']  # 클래스 이름 리스트
+
+# 업로드 HTML 렌더링
+@app.route('/upload')
+def render_file():
+    return render_template('upload.html')
+
+
+# 파일 업로드 처리
+@app.route('/fileUpload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        # 저장할 경로 + 파일명
+        f.save(secure_filename(f.filename))
+
+        # 예측할 이미지를 읽어오고 전처리 작업 수행
+        image = Image.open(f)
+        image = image.resize((224, 224))
+        image = np.expand_dims(image, axis=0)
+        image = tf.keras.applications.mobilenet_v2.preprocess_input(image)
+
+        # 예측 수행
+        prediction = model.predict(image)
+        class_index = np.argmax(prediction)  # 가장 높은 확률 값을 가진 클래스의 인덱스
+        class_name = class_names[class_index]  # 클래스 이름
+        result = {'class_name': class_name}  # 클래스 이름 반환
+        return jsonify(result), 200
+
+
+if __name__ == '__main__':
+    # 서버 실행
+    app.run(debug=True)
---
+++ README.md
@@ -0,0 +1,2 @@
+# CBNU-OpenSourceProject
+충북대학교 오픈소스 전문프로젝트
